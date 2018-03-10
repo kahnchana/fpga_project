1a7a4e43525a9030155c4374f959c9d60f1e3329
Kanchana Ranasinghe <kahnchana@gmail.com>
2018-03-10 12:13:47 +0530

    update

--- README.md
+++ README.md
@@ -1,3 +1,23 @@
 # fpga_project
 
+## Description 
 processor.v contains the outline for a simple processor. 
+Used IP cores to build counter (PC) and ALU. Can have upto 16 micro instructions. 
+
+## To Do
+Finalize ISA
+Code Control Units Logic (the micro instructions)
+Test the current architecture of processor
+Create RAM to load images and data to
+Algorithm to down-sample a given image
+
+## Proposed ISA instruction set
+1) mv X Y
+2) mvi X, Din
+3) add X, Y
+4) sub X, Y
+5) load X, [Y]  (load the data in memory location Y to register X)
+6) store X, [y] (store the data in register X to memory location Y)
+7) mvnz X, Y (move Y to X when ALU output is not nil: can be used to implement while loops easily)
+
+Can add more instructions here if we need. 
--- processor.v
+++ processor.v
@@ -3,7 +3,7 @@
 //  Written by Kanchana Ranasinghe 
 //=======================================================
 
-// Simple Processor (proc)
+// Processor (proc)
 // Data can be obtained from registers, or the data input from DIn
 // Data can be written to registers, or obtained from BusWires by other module
 // Addition and Substraction operations can be performed
@@ -15,36 +15,46 @@
 // Includes only move, obtain data, add, subtract micro-instructions
 // Can add upto 16 micro instructions
 
-module processor (DIN, Resetn, Clock, Run, Done, BusWires);
+module processor (DIN, Resetn, Clock, Run, Done, BusWires, ADDR, DOUT, W, Tstep_Q, PC);
   input 			[15:0] 			DIN;  // data in to processor (from memory)
   input 								Resetn;  // resets counter in middle of instruction
   input 								Clock;  // clock input for register
   input 								Run;  // should be high for processor to run
   output reg 						Done;  // indicates end of instruction 
   output reg 	[15:0] 			BusWires;  // outputs values on bus wires
+  output  		[15:0]			ADDR;  // address register to access memory 
+  output  		[15:0]			DOUT;  // data register to write to memory 
+  output reg						W;  // write enable for memory
+  output 		[2:0]				Tstep_Q;  // internal counter
+  output 		[15:0]			PC;  // program counter
 
   //declare registers and wires
-  reg 				IRin, DINout, Ain, Gout, Gin;  // enable bits for registers
-  reg					AddSub;  //enable bit for subtraction in ALU 
+  reg 				IRin, DINout, Ain, Gout, Gin, ADDRin, DOUTin;  // enable bits for registers
+  reg					AddSub;  // enable bit for subtraction in ALU 
+  reg 				incr_pc;  // enable bit to increase counter
   reg 	[7:0] 	Rout;  // selection register for MUX (to pick GP register) 
-  reg 	[7:0] 	Rin;  // enable bits for 8 general purpose registors
+  reg 	[7:0] 	Rin;  // enable bits for PC (R[0]) and 7 general purpose registers
   reg 	[9:0] 	MUXsel;  // multiplexer output
+  
   wire 	[7:0] 	Xreg, Yreg;  // one-hot value for data registers of instruction
   wire 	[1:9] 	IR;  // instruction regiser
   wire 	[1:3] 	I;  // instruction part of instruction
   wire 	[15:0] 	R0, R1, R2, R3, R4, R5, R6, R7;  //general purpose registers
   wire 	[15:0] 	A, G;  //output of ALU input and output registers
   wire 	[15:0]	result;  //ALU out to G connection
-  wire 	[1:0] 	Tstep_Q;  // counter for instructions taking multiple clock cycles
+  wire 	[2:0] 	Tstep_Q;  // counter for instructions taking multiple clock cycles
 
   wire Clear = Done || ~Resetn;  // clear resets the counter
   upcount Tstep (Clear, Clock, Tstep_Q);  // instantiates the counter 
   
   assign I = IR[1:3];  //obtains instruction part from IR
+  
   dec3to8 decX (IR[4:6], 1'b1, Xreg);  // converts register addresses to one-hot
   dec3to8 decY (IR[7:9], 1'b1, Yreg);  // converts register addresses to one-hot
-  
-  // obtain inputs and define Control Units (CU) logic
+  // one-hot encoding used to simplify multiplexer 
+ 
+ 
+  // obtain inputs and define Control Unit's (CU) Micro Instructions
   always @(Tstep_Q or I or Xreg or Yreg)
   begin
     // specify initial values (all set to zero)
@@ -56,16 +66,22 @@ module processor (DIN, Resetn, Clock, Run, Done, BusWires);
     Gout = 1'b0;
     Gin = 1'b0;
     AddSub = 1'b0;
-
+	 DouTin = 1'b0;
+    ADDRin = 1'b0;
+    W_D = 1'b0;
+    incr_pc = 1'b0;
     Done = 1'b0;
 
+	 // begin counter and defines micro instructions
+	 // need to make changes here: forgot PC :-p
+	 
     case (Tstep_Q)
-      2'b00: // store DIN in IR in time step 0
+      3'b000: // store DIN in IR in time step 0
       begin
 			if (Run) IRin = 1'b1;  // setting enable bit to one activates register
 			else Done = 1'b1;
       end
-      2'b01: // define signals in time step 1
+      3'b001: // define signals in time step 1
         case (I)
           3'b000:  // mv 
           begin
@@ -90,7 +106,7 @@ module processor (DIN, Resetn, Clock, Run, Done, BusWires);
             Ain = 1'b1;  // bus data stored to A
           end
         endcase
-      2'b10: // define signals in time step 2
+      3'b010: // define signals in time step 2
         case (I)
           3'b010:  // add
           begin
@@ -104,7 +120,7 @@ module processor (DIN, Resetn, Clock, Run, Done, BusWires);
             Gin = 1'b1;  // ALU output loaded to G             
           end
         endcase
-      2'b11: //define signals in time step 3
+      3'b011: //define signals in time step 3
         case (I)
           3'b010:  //add
           begin
@@ -123,7 +139,7 @@ module processor (DIN, Resetn, Clock, Run, Done, BusWires);
   end
 
   // create general purpose registers 
-  regn reg_0 (BusWires, Rin[0], Clock, R0);
+  counter_lpm reg_PC (1'b1, Clock, incr_pc, BusWires, ~resetn, Rin[0], Clock, PC);
   regn reg_1 (BusWires, Rin[1], Clock, R1);
   regn reg_2 (BusWires, Rin[2], Clock, R2);
   regn reg_3 (BusWires, Rin[3], Clock, R3);
@@ -140,6 +156,12 @@ module processor (DIN, Resetn, Clock, Run, Done, BusWires);
   regn reg_A (BusWires, Ain, Clock, A);
   regn reg_G (result, Gin, Clock, G);
 
+  // create memory access registers
+  regn reg_ADDR (BusWires, ADDRin, Clock, ADDR);
+  regn reg_DOUT (BusWires, DOUTin, Clock, DOUT);
+  regn reg_W (W_D, 1'b1, Clock, W);
+  defparam reg_W.n = 1; 
+  
   // create ALU (using IP core)
   addsub AS (~AddSub, A, BusWires, result);
 
@@ -152,10 +174,10 @@ module processor (DIN, Resetn, Clock, Run, Done, BusWires);
     MUXsel[0] = DINout;
     
     case (MUXsel)
-      10'b0000000001: BusWires = DIN;
-      10'b0000000010: BusWires = G;
-      10'b0000000100: BusWires = R0;
-      10'b0000001000: BusWires = R1;
+      10'b0000000001: BusWires = DIN; // Data from memory
+      10'b0000000010: BusWires = G;   // ALU output
+      10'b0000000100: BusWires = R0;  // R0 is PC
+      10'b0000001000: BusWires = R1;  // rest are general purpose registers
       10'b0000010000: BusWires = R2;
       10'b0000100000: BusWires = R3;
       10'b0001000000: BusWires = R4;
@@ -171,12 +193,12 @@ endmodule
 // create a timer for instructions taking multiple clock cycles
 module upcount(Clear, Clock, Q);
   input Clear, Clock;
-  output [1:0] Q;
-  reg [1:0] Q;
+  output [2:0] Q;
+  reg [2:0] Q;
 
   always @(posedge Clock)
     if (Clear)
-      Q <= 2'b0;
+      Q <= 3'b0;
     else
       Q <= Q + 1'b1;
 endmodule
